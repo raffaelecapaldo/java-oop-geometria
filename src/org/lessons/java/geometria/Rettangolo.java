880a08dea1b45c7b7bc83e4b1031ad834933b2be
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-15 14:50:55 +0200

    Added methods

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -2,4 +2,22 @@ package org.lessons.java.geometria;
 
 public class Rettangolo {
 
+	int base;
+	int height;
+	
+	Rettangolo(int base, int height) {
+		this.base = base;
+		this.height  = height;
+	}
+	
+	public int calcoloArea() {
+		int area = base * height;
+		return area;
+	}
+	
+	public int calcolaPerimetro() {
+		int perimetro = (base + height) * 2;
+		return perimetro;
+	}
+	
 }
