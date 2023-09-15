bd4325e50d1b6cefae392f821e0865a3a016e82a
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-15 15:21:35 +0200

    Exercise completed

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,10 +1,32 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Rettangolo[] rettangoli = new Rettangolo[3];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.print("Inserisci base rettangolo " + (i+1) + ": ");
+			int base = sc.nextInt();
+			System.out.print("Inserisci area rettangolo " + (i+1) + ": ");
+			int height = sc.nextInt();
+			
+			rettangoli[i] = new Rettangolo(base, height);
+		}
+		
+		for (int i = 0; i < rettangoli.length; i++) {
+			rettangoli[i].calcolaPerimetro();
+			rettangoli[i].calcoloArea();
+			rettangoli[i].printInfo(i+1);
+			rettangoli[i].printMath();
+			System.out.println("\n-------------------------");
+		}
+		
+		
 	}
 
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -4,6 +4,8 @@ public class Rettangolo {
 
 	int base;
 	int height;
+	int area;
+	int perimetro;
 	
 	Rettangolo(int base, int height) {
 		this.base = base;
@@ -12,12 +14,26 @@ public class Rettangolo {
 	
 	public int calcoloArea() {
 		int area = base * height;
+		this.area = area;
 		return area;
 	}
 	
 	public int calcolaPerimetro() {
 		int perimetro = (base + height) * 2;
+		this.perimetro = perimetro;
 		return perimetro;
+	}
+	
+	public void printInfo(int id) {
+		String info = "Info rettangolo " + id + "\nBase: " + this.base +
+					" - Altezza: " + this.height;
+		
+		System.out.println(info);
+	}
+	
+	public void printMath() {
+		String info = "Area: " + this.area + " - Perimetro: " + this.perimetro;
+		System.out.println(info);
 	}
 	
 }
